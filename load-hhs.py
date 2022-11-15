16e01d6ba2b6dbc045e0093073c97cd097794f85
xcsun3 <lyndonsun798@yahoo.com>
2022-11-15 16:32:07 -0500

    Added function file

--- load-hhs.py
+++ load-hhs.py
@@ -1,11 +1,8 @@
-"""Function and Driver file to load and clean data."""
+"""Driver file to load and clean weekly HHS data."""
+
 import sys
 import pandas as pd
-
-
-def convert_to_pd_dataframe(file_path):
-    return pd.read_csv(file_path)
-
+from cleandata import convert_to_pd_dataframe
 
 weekly_tbl = convert_to_pd_dataframe("./data/hhs_weekly/" + str(sys.argv[1]))
 print(weekly_tbl)
