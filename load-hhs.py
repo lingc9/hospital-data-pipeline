4ced84b14587b50b97ab46c0bf039c5770aef441
Carol Ling <lingcarol9@gmail.com>
2022-11-15 16:25:31 -0500

    Script will run when given a command line prompt as specified.

--- load-hhs.py
+++ load-hhs.py
@@ -1,1 +1,11 @@
 """Function and Driver file to load and clean data."""
+import sys
+import pandas as pd
+
+
+def convert_to_pd_dataframe(file_path):
+    return pd.read_csv(file_path)
+
+
+weekly_tbl = convert_to_pd_dataframe("./data/hhs_weekly/" + str(sys.argv[1]))
+print(weekly_tbl)
