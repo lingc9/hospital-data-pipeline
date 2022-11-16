5e73a84fcf89d14384f99d705004f5fa16fe955f
xcsun3 <lyndonsun798@yahoo.com>
2022-11-16 00:19:23 -0500

    Helper functions for loading data

---
+++ load-quality.py
@@ -0,0 +1,50 @@
+"""Driver file to load and clean Hospital General Information data."""
+
+import sys
+import time
+import datetime
+from cleandata import clean_quality_data
+from loaddata import connect_to_sql, load_hospital_info
+
+
+nfile = "./data/hospital_quality/" + str(sys.argv[2])
+insert_data = clean_quality_data(nfile)
+
+collect_date = str(sys.argv[1])
+collect_date = datetime.datetime.strptime(collect_date, "%Y-%m-%d")
+
+# Subset data to insert (Testing Purposes)
+insert_data = insert_data.iloc[0:10, ]
+print(insert_data)
+
+# Start Insertion
+num_rows_inserted = 0
+failed_insertion = []
+conn = connect_to_sql()
+
+with conn.transaction():
+    for i in range(insert_data.shape[0]):
+        data = insert_data.loc[int(i), ]
+        try:
+            with conn.transaction():
+                load_hospital_info(conn, data, collect_date)
+        except Exception:
+            failed_insertion.append(i)
+            print("Insertion failed at line " + str(i))
+        else:
+            num_rows_inserted += 1
+
+print("Read in " + str(insert_data.shape[0]) + " lines in total")
+print("Successfully added " + str(num_rows_inserted))
+
+# Output csv with lines that failed to insert
+if failed_insertion:
+    failed_lines = insert_data.iloc[failed_insertion]
+    curr_time = time.strftime("%H_%M_%S", time.localtime())
+    fname = "./data/hospital_quality/" + curr_time + "_failed_insertion.csv"
+    failed_lines.to_csv(fname)
+    print("Saved lines that failed to insert in " + fname)
+
+# Only run these part if all is done, make some kind of fail save.
+conn.commit()
+conn.close()
---
+++ loaddata.py
@@ -0,0 +1,110 @@
+"""
+Functions that connect to psql server and load the data
+
+
+connect_to_sql - creats a connection to postgresql
+
+load_hospital_info - load a row of data into hospital_info table
+
+load_hospital_data - load a row of data into hospital_data table
+
+load_hospital_location - load a row of data into hospital_location table
+
+
+Arguments:
+
+conn - psycopg connect to postgresql server
+
+data - a row of pd data frame containing the data to be inserted
+
+collect_date - the date when the quality file is collected
+"""
+
+import psycopg
+
+import credentials
+
+
+def connect_to_sql():
+    conn = psycopg.connect(
+        host="sculptor.stat.cmu.edu", dbname=credentials.DB_USER,
+        user=credentials.DB_USER, password=credentials.DB_PASSWORD
+    )
+    return conn
+
+
+def load_hospital_info(conn, data, collect_date):
+    cur = conn.cursor()
+
+    cur.execute("INSERT INTO hospital_info (hospital_id, name, hospital_type, "
+                "ownership, collection_date, state, address, city, zip, "
+                "emergency_service, quality_rating)"
+                "VALUES (%(hospital_id)s, %(name)s, %(hospital_type)s, "
+                "%(ownership)s, %(collection_date)s, %(state)s, "
+                "%(address)s, %(city)s, %(zip)s, %(emergency_service)s, "
+                "%(quality_rating)s)",
+                {
+                 "hospital_id": data["hospital_id"],
+                 "name": data["name"],
+                 "hospital_type": data["hospital_type"],
+                 "ownership": data["ownership"],
+                 "collection_date": collect_date,
+                 "state": data["state"],
+                 "address": data["address"],
+                 "city": data["city"],
+                 "zip": data["zip"],
+                 "emergency_service": data["emergency_service"],
+                 "quality_rating": data["quality_rating"],
+                }
+                )
+
+    return True
+
+
+def load_hospital_data(conn, data, collect_date):
+    cur = conn.cursor()
+
+    cur.execute("INSERT INTO hospital_data (hospital_id, collection_date, "
+                "avalible_adult_beds, avalible_pediatric_beds, "
+                "occupied_adult_beds, occupied_pediatric_beds, "
+                "available_ICU_beds, occupied_ICU_beds, "
+                "COVID_beds_use, COVID_ICU_use)"
+                "VALUES (%(hospital_id)s, %(collection_date)s, "
+                "%(avalible_adult_beds)s, %(avalible_pediatric_beds)s, "
+                "%(occupied_adult_beds)s, %(occupied_pediatric_beds)s, "
+                "%(available_ICU_beds)s, %(occupied_ICU_beds)s, "
+                "%(COVID_beds_use)s, %(COVID_ICU_use)s)",
+                {
+                 "hospital_id": data["hospital_id"],
+                 "collection_date": data["collection_date"],
+                 "avalible_adult_beds": data["avalible_adult_beds"],
+                 "avalible_pediatric_beds": data["avalible_pediatric_beds"],
+                 "occupied_adult_beds": data["occupied_adult_beds"],
+                 "occupied_pediatric_beds": data["occupied_pediatric_beds"],
+                 "available_ICU_beds": data["available_ICU_beds"],
+                 "occupied_ICU_beds": data["occupied_ICU_beds"],
+                 "COVID_beds_use": data["COVID_beds_use"],
+                 "COVID_ICU_use": data["COVID_ICU_use"]
+                }
+                )
+
+    return True
+
+
+def load_hospital_location(conn, data, collect_date):
+    cur = conn.cursor()
+
+    cur.execute("INSERT INTO hospital_location (hospital_id, "
+                "collection_date, fips, latitude, longitude)"
+                "VALUES (%(hospital_id)s, %(collection_date)s, "
+                " %(fips)s, %(latitude)s, %(longitude)s)",
+                {
+                 "hospital_id": data["hospital_id"],
+                 "collection_date": data["collection_date"],
+                 "fips": data["fips"],
+                 "latitude": data["latitude"],
+                 "longitude": data["longitude"]
+                }
+                )
+
+    return True
